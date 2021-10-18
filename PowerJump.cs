066d56c4099adcf50b9ffc42d783ac58743ed200
Zixed11 <83886612+Zixed11@users.noreply.github.com>
2021-10-18 14:21:56 -0400

    Create PowerJump.cs

---
+++ PowerJump.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using HarmonyLib;
+using BepInEx;
+using UnityEngine;
+using System.Reflection;
+using UnityEngine.XR;
+using Photon.Pun;
+using System.IO;
+using System.Net;
+using Photon.Realtime;
+using UnityEngine.Rendering;
+
+
+
+namespace PowerJump
+{
+    [BepInPlugin("org.ZixedTheMonkeGuy.monkeytag.PowerJump", "Power Jump", "0.1.0.0")]
+    public class MyPatcher : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            var harmony = new Harmony("com.ZixedTheMonkeGuy.monkeytag.PowerJump");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    [HarmonyPatch(typeof(GorillaLocomotion.Player))]
+    [HarmonyPatch("Update", MethodType.Normal)]
+    public class Class1
+    {
+        static bool powerJump = false;
+
+        static void Postfix(GorillaLocomotion.Player __instance)
+        {
+            if (!PhotonNetwork.CurrentRoom.IsVisible || !PhotonNetwork.InRoom)
+            {
+                List<InputDevice> list = new List<InputDevice>();
+                InputDevices.GetDevicesWithCharacteristics(UnityEngine.XR.InputDeviceCharacteristics.HeldInHand | UnityEngine.XR.InputDeviceCharacteristics.Right | UnityEngine.XR.InputDeviceCharacteristics.Controller, list);
+                list[0].TryGetFeatureValue(CommonUsages.gripButton, out powerJump); 
+
+                    if (powerJump)
+                    {
+                         __instance.maxJumpSpeed = 5.5f;
+                    }
+                    else
+                    {
+                         __instance.maxJumpSpeed = 1.1f;
+                    }
+                }
+                
+            }
+        }
+    }
